8b7bb944807de8848129f54cfcbf4706a7e9ac72
micka <mickael.gerard.dev@gmail.com>
2022-01-31 14:00:30 +0100

    add import os  & dotenv

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 
 import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="config")
+
+
 
 client = discord.Client()
+client.run(os.getenv("TOKEN"))
+
+@client.event
+async def on_ready():
+    print("Le bot est prêt !")
+
+async def on_message(message):
+    print(message.content)
